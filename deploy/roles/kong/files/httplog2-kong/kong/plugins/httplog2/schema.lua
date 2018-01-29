7cf734c38a1d0b169f1bbd5b000f924a1ad8e1e8
Leandro Gomez <lgomez@devartis.com>
2018-01-29 10:07:22 -0300

    Agrego plugin de httplog

---
+++ deploy/roles/kong/files/httplog2-kong/kong/plugins/httplog2/schema.lua
@@ -0,0 +1,11 @@
+return {
+  no_consumer = true, -- this plugin will only be API-wide,
+  fields = {
+    endpoint = {type = "string", required = true},
+    -- Describe your plugin's configuration's schema here.
+  },
+  self_check = function(schema, plugin_t, dao, is_updating)
+    -- perform any custom verification
+    return true
+  end
+}
