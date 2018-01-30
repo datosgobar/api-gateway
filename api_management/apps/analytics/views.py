54aa9ac9a58bc155f916e150b2e2ef149c33d4d1
Sebastian Gonzalez <sgonzalez@devartis.com>
2018-01-30 11:44:08 -0300

    agregado decorador para indicar autenticacion por token en create_query
    agregado decorador para indicar autenticacion por token en create_query
    
        refs:#8
    
        refs:#8
    
        refs:#8
    
        refs:#8

--- api_management/apps/analytics/views.py
+++ api_management/apps/analytics/views.py
@@ -1,12 +1,15 @@
 import json
 
 from rest_framework import status
-from rest_framework.decorators import permission_classes, api_view
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import permission_classes, api_view, \
+    authentication_classes
 from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 
 
 @api_view(['POST'])
+@authentication_classes((TokenAuthentication,))
 @permission_classes((IsAdminUser,))
 def create_query(request):
     try:
