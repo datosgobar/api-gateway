9d65ff512d0f5504bbf62ffc22c1aa37b4e89e45
Sebastian Gonzalez <sgonzalez@devartis.com>
2018-01-30 12:54:01 -0300

    agregado manejo de json al crear una query por rest
    agregada creacion de querys en create_query
    
        refs:#8
    
        refs:#8
    
        refs:#8
    
        refs:#8

--- api_management/apps/analytics/views.py
+++ api_management/apps/analytics/views.py
@@ -1,11 +1,14 @@
-import json
-
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import permission_classes, api_view, \
     authentication_classes
 from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
+from rest_framework.exceptions import ParseError
+from rest_framework.parsers import JSONParser
+from django.utils.six import BytesIO
+
+from api_management.apps.analytics.serializers import QuerySerializer
 
 
 @api_view(['POST'])
@@ -13,11 +16,20 @@ from rest_framework.response import Response
 @permission_classes((IsAdminUser,))
 def create_query(request):
     try:
-        json.loads(request.body)
-    except json.JSONDecodeError:
+        content = request.body
+        stream = BytesIO(content)
+        data = JSONParser().parse(stream)
+    except ParseError:
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
+    serializer = QuerySerializer(data=data)
+
+    if serializer.is_valid():
+        query = serializer.save()
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     content = {
-        'status': 'request was permitted'
+        'query': query,
     }
-    return Response(content, status=status.HTTP_200_OK)
+    return Response(content, status=status.HTTP_201_CREATED)
