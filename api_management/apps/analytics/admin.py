3b9d9a7cb806f3f81320a198046dbf6eaa1dd2cb
Sebastian Gonzalez <sgonzalez@devartis.com>
2018-01-30 10:54:21 -0300

    agregado admin de tokens
        refs:#8
    
        refs:#8

--- api_management/apps/analytics/admin.py
+++ api_management/apps/analytics/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
+from rest_framework.authtoken.admin import TokenAdmin
+
 from .models import Query
 
 admin.site.register(Query)
+
+TokenAdmin.raw_id_fields = ('user',)
