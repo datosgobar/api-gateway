c3b33159a396f56d864bc622cb34f3222e6f5f96
Leandro Gomez <lgomez@devartis.com>
2018-01-30 16:52:48 -0300

    Agrego excepcion de validacion en pylint
        refs:#8
    
        refs:#8

--- api_management/apps/analytics/urls.py
+++ api_management/apps/analytics/urls.py
@@ -2,7 +2,7 @@ from rest_framework import routers
 
 from .views import QueryViewSet
 
-router = routers.SimpleRouter()
+router = routers.SimpleRouter()  # pylint: disable=invalid-name
 router.register(r'queries', QueryViewSet)
 
-urlpatterns = router.urls
+urlpatterns = router.urls  # pylint: disable=invalid-name
