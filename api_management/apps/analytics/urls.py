96f72a07d51e212e739dd7dab83d25c5bb1af1b6
Sebastian Gonzalez <sgonzalez@devartis.com>
2018-01-30 10:36:57 -0300

    agregada url para generar tokens por usuarios
        refs:#8
    
        refs:#8

--- api_management/apps/analytics/urls.py
+++ api_management/apps/analytics/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url, include
 from django.urls import path
 
+from rest_framework.authtoken import views as authtoken_views
+
 from .views import create_query
 
 urlpatterns = [  # pylint: disable=invalid-name
     path('queries/', create_query),
     url(r'^auth/', include('djoser.urls')),
     url(r'^auth/', include('djoser.urls.authtoken')),
+    url(r'^token/', authtoken_views.obtain_auth_token)
 ]
