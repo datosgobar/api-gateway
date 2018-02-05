e2c4f07cf7876b4430c0732e3f3781178818ad11
Sebastian Gonzalez <sgonzalez@devartis.com>
2018-02-05 11:52:45 -0300

    agrego comunicacion con server kong al manejar modelos de apis
        refs:#18

--- api_management/apps/api_registry/models.py
+++ api_management/apps/api_registry/models.py
@@ -1,8 +1,62 @@
 from django.db import models
 
+import kong.client
+
+KONG_ADMIN_URL = 'http://localhost:8001/'
+
+
+class ApiManager:
+
+    @staticmethod
+    def _kong_client():
+        return kong.client.APIAdminClient(KONG_ADMIN_URL)
+
+    @classmethod
+    def manage(cls, api_instance):
+        if api_instance.enabled:
+            if api_instance.kong_id:
+                cls.update(api_instance)
+            else:
+                cls.create(api_instance)
+        elif api_instance.kong_id:
+            cls.delete(api_instance)
+
+    @classmethod
+    def update(cls, api_instance):
+        client = ApiManager._kong_client()
+        fields = {"name": api_instance.name,
+                  "uris": api_instance.uri,
+                  }
+        client.update(api_instance.kong_id, api_instance.upstream_url, **fields)
+
+    @classmethod
+    def create(cls, api_instance):
+        client = ApiManager._kong_client()
+        response = client.create(api_instance.upstream_url,
+                                 name=api_instance.name,
+                                 request_path=api_instance.uri,
+                                 strip_request_path=True)
+        api_instance.kong_id = response['id']
+
+    @classmethod
+    def delete(cls, api_instance):
+        client = ApiManager._kong_client()
+        client.delete(api_instance.kong_id)
+        api_instance.kong_id = None
+
 
 class Api(models.Model):
     name = models.CharField(unique=True, max_length=200)
     upstream_url = models.URLField()
     uri = models.CharField(max_length=200)
     enabled = models.BooleanField()
+    kong_id = models.CharField(max_length=100, null=True)
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        ApiManager.manage(self)
+        return super(Api, self).save(force_insert, force_update, using, update_fields)
+
+    def delete(self, using=None, keep_parents=False):
+        ApiManager.delete(self)
+        return super(Api, self).delete(using, keep_parents)
