bcb338c14d933366a833dab52ef4e788aa016362
Sebastian Gonzalez <sgonzalez@devartis.com>
2018-02-05 11:51:42 -0300

    agrego admin de apis
        refs:#18

--- api_management/apps/api_registry/admin.py
+++ api_management/apps/api_registry/admin.py
@@ -2,4 +2,24 @@ from django.contrib import admin
 
 from .models import Api
 
-admin.site.register(Api)
+
+@admin.register(Api)
+class ApiAdmin(admin.ModelAdmin):
+    fieldsets = ('name', 'upstream_url', 'uri', 'enabled', 'kong_id')
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'upstream_url', 'uri', 'enabled')
+        }),
+        ('Advanced', {
+            'classes': ('collapse',),
+            'fields': ('kong_id',),
+        }),
+    )
+
+    def add_view(self, request, form_url='', extra_context=None):
+        self.readonly_fields = ('kong_id',)
+        return super(ApiAdmin, self).add_view(request, form_url, extra_context)
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        self.readonly_fields = ('name', 'kong_id')
+        return super(ApiAdmin, self).change_view(request, object_id, form_url, extra_context)
