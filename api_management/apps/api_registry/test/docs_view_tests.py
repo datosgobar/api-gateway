108d6f9c33f853affd899339f66aa576eed5d149
Sebastian Gonzalez <sgonzalez@devartis.com>
2018-02-26 15:39:23 -0300

    Agrego mock de requests a los test
        refs:#32
    
        refs:#32

--- api_management/apps/api_registry/test/docs_view_tests.py
+++ api_management/apps/api_registry/test/docs_view_tests.py
@@ -1,3 +1,4 @@
+import requests_mock
 import faker
 
 from rest_framework import status
@@ -7,7 +8,7 @@ from api_management.apps.api_registry.models import ApiData
 from api_management.libs.providers.providers import CustomInfoProvider
 
 
-class AccountTests(APITestCase):
+class DocsViewTests(APITestCase):
 
     def setUp(self):
         self.faker = faker.Faker()
@@ -15,10 +16,19 @@ class AccountTests(APITestCase):
 
         self.api_data = ApiData(name=self.faker.api_name(),
                                 upstream_url=self.faker.url(),
-                                uris=self.faker.api_path())
+                                uris=self.faker.api_path(),
+                                documentation_url=self.faker.url())
         self.api_data.save()
 
-    def test_doc_view(self):
+    @requests_mock.mock()
+    def test_doc_view(self, mock):
+        # Setup
+        mock.get(self.api_data.documentation_url, json={'data': 'value'})
+
+        # Exercise
         url = '/api/registry/docs/%s/' % self.api_data.name
         response = self.client.get(url)
+
+        # Verify
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json(), {'data': 'value'})
