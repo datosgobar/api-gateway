0301e64893dd0f3e5e99d2c298dae30a93247c64
Sebastian Gonzalez <sgonzalez@devartis.com>
2018-02-27 14:57:27 -0300

    Ahora la documentacion se muestra en html
        refs:#32
    
        refs:#32

--- api_management/apps/api_registry/views.py
+++ api_management/apps/api_registry/views.py
@@ -1,23 +1,45 @@
+from json import JSONDecodeError
 import requests
 from requests.exceptions import ConnectionError  # pylint: disable=redefined-builtin
+from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+from django.core.exceptions import ObjectDoesNotExist
 from .models import ApiData
 
 
 class DocsView(APIView):
 
+    renderer_classes = (TemplateHTMLRenderer,)
+    template_name = "404.html"
+
     @staticmethod
     def get(_, name):
-        url = ApiData.objects.get(name=name).documentation_url
+        try:
+            url = ApiData.objects.get(name=name).documentation_url
+        except ObjectDoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        if not url:
+            data = {'message': 'doc server not configured'}
+            return Response(data, status=status.HTTP_404_NOT_FOUND)
+
         try:
             response = requests.get(url)
         except ConnectionError:
-            data = 'Unable to connect to documentation server'
-        else:
-            if response.status_code == status.HTTP_200_OK:
-                data = response.json()
-            else:
-                data = 'Unable to retrieve documentation'
-        return Response(data)
+            data = {'message': 'Unable to connect to documentation server'}
+            return Response(data, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        if response.status_code != status.HTTP_200_OK:
+            data = {'message': 'Unable to retrieve documentation'}
+            return Response(data, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        try:
+            json = response.json()
+        except JSONDecodeError:
+            data = {'message': "doc server response can't be json decoded"}
+            return Response(data, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        data = {'api_name': name, 'data': json}
+        return Response(data, template_name="api_documentation.html")
--- conf/settings/base.py
+++ conf/settings/base.py
@@ -81,7 +81,7 @@ ROOT_URLCONF = 'conf.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
