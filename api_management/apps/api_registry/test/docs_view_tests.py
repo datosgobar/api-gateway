296258087946df6354942b57b88ca68c45ec1cc1
Sebastian Gonzalez <sgonzalez@devartis.com>
2018-02-28 14:36:12 -0300

    Refactor: ahora los tests no requieren un server kong
        refs:#32
    
        refs:#32

--- api_management/apps/api_registry/test/docs_view_tests.py
+++ api_management/apps/api_registry/test/docs_view_tests.py
@@ -1,3 +1,5 @@
+import requests_mock
+
 import faker
 
 from rest_framework import status
@@ -9,19 +11,31 @@ from api_management.libs.providers.providers import CustomInfoProvider
 
 class DocsViewTests(APITestCase):
 
-    def setUp(self):
+    @requests_mock.mock()
+    def setUp(self, request_mock):  # pylint: disable=arguments-differ
+
         self.faker = faker.Faker()
         self.faker.add_provider(CustomInfoProvider)
 
-        self.api_data = ApiData(name=self.faker.api_name(),
-                                upstream_url=self.faker.url(),
-                                uris=self.faker.api_path(),
-                                documentation_url=self.faker.url())
+        self.api_data_dict = {'name': self.faker.api_name(),
+                              'upstream_url': self.faker.url(),
+                              'uris': self.faker.api_path(),
+                              'documentation_url': self.faker.url()}
+
+        self.api_data = ApiData(**self.api_data_dict)
+
+        request_mock.post(requests_mock.ANY,
+                          status_code=status.HTTP_201_CREATED,
+                          json=self.api_data_dict)
+
         self.api_data.save()
 
         self.api_doc_url = '/api/registry/docs/%s/' % self.api_data.name
 
-    def tearDown(self):
+    @requests_mock.mock()
+    def tearDown(self, request_mock):  # pylint: disable=arguments-differ
+        request_mock.delete(requests_mock.ANY, status_code=status.HTTP_204_NO_CONTENT)
+
         self.api_data.delete()
 
     def test_doc_view(self):
