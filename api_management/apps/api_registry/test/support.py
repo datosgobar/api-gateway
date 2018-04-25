579bf870ac713ba192bc0f29b1b079c4c7aeeccd
Leandro Gomez <lgomez@devartis.com>
2018-04-25 14:01:57 -0300

    Fix pylint

--- api_management/apps/api_registry/test/support.py
+++ api_management/apps/api_registry/test/support.py
@@ -1,10 +1,12 @@
 from api_management.apps.api_registry.models import KongApi
 
 
+# pylint: disable=invalid-name
+
 def generate_api_data(name, upstream_url, uris, kong_id, api_id):
     api = KongApi(name=name,
                   upstream_url=upstream_url,
                   uri=uris,
                   kong_id=kong_id)
-    api.id = api_id  # pylint: disable=invalid-name
+    api.id = api_id
     return api
