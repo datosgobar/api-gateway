10e86a4d04a6b66179bc65a6d5b0e98db35ec950
Alejandro Sobko <alejandro.sobko@gmail.com>
2018-09-25 12:45:30 -0300

    Agrego task para generar los analytics (WIP)

--- api_management/apps/analytics/tasks.py
+++ api_management/apps/analytics/tasks.py
@@ -1,4 +1,7 @@
 from django_rq import job
+from datetime import timedelta
+from api_management.apps.analytics.csv_generator import CsvGenerator
+from api_management.apps.api_registry.models import KongApi
 
 
 @job('create_model')
@@ -6,3 +9,13 @@ def make_model_object(data, serializer_class):
     serializer = serializer_class(data=data)
     serializer.is_valid(raise_exception=True)
     serializer.save()
+
+
+@job('generate_analytics')
+def generate_analytics_dump(date=None):
+    if date is None:
+        date = (date.today() - timedelta(1)).strftime('%Y-%m-%d')
+
+    for api in KongApi.objects.all():
+        csv_generator = CsvGenerator(api_name=api.name, date=date)
+        csv_generator.generate()
