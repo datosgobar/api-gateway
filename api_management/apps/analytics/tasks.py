6a6e19b0c163046ff588ca8da3867a3a8e236556
Alejandro Sobko <alejandro.sobko@gmail.com>
2018-09-27 11:52:45 -0300

    Agrego generate_analytics a django-rq

--- api_management/apps/analytics/tasks.py
+++ api_management/apps/analytics/tasks.py
@@ -1,5 +1,7 @@
+from datetime import date
+from dateutil import relativedelta
 from django_rq import job
-from datetime import timedelta
+
 from api_management.apps.analytics.csv_generator import CsvGenerator
 from api_management.apps.api_registry.models import KongApi
 
@@ -11,11 +13,11 @@ def make_model_object(data, serializer_class):
     serializer.save()
 
 
-@job('generate_analytics')
-def generate_analytics_dump(date=None):
-    if date is None:
-        date = (date.today() - timedelta(1)).strftime('%Y-%m-%d')
+@job('generate_analytics', timeout=3600)
+def generate_analytics_dump(date_string=None):
+    if date_string is None:
+        date_string = date.today() - relativedelta.relativedelta(days=1)
 
     for api in KongApi.objects.all():
-        csv_generator = CsvGenerator(api_name=api.name, date=date)
+        csv_generator = CsvGenerator(api_name=api.name, date=date_string)
         csv_generator.generate()
