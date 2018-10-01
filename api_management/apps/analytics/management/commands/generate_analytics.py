41f575a498c91529dc907d996c9e74db101d7df3
Alejandro Sobko <alejandro.sobko@gmail.com>
2018-10-01 13:40:50 -0300

    Agrego CsvAnalyticsGeneratorTask al admin de django

--- api_management/apps/analytics/management/commands/generate_analytics.py
+++ api_management/apps/analytics/management/commands/generate_analytics.py
@@ -41,9 +41,9 @@ class Command(BaseCommand):
         self.stdout.write(f"Generando csv para el día {analytics_date.date()}...")
         try:
             generate_analytics_dump(analytics_date)
-            task.logs += "Archivo csv de analytics generado correctamente."
+            task.logs += f"Archivo csv de analytics generado correctamente para el día {analytics_date}."
             task.save()
         except Exception as e:
-            task.logs += f"Error generando csv de analytics: {e}"
+            task.logs += f"Error generando csv de analytics para el día {analytics_date}: {e}"
             task.save()
             raise e
