f896ec750c85d5e0f0d1fe7c012dae91e94312f1
Alejandro Sobko <alejandro.sobko@gmail.com>
2018-10-17 12:39:41 -0300

    Merge de #149

--- api_management/apps/analytics/models.py
+++ api_management/apps/analytics/models.py
@@ -4,7 +4,6 @@ import uuid
 from urllib.parse import parse_qsl, urlparse
 
 import requests
-from django.conf import settings
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
