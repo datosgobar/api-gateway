6ed5d60d92e0d68272f5aff71ee5cb529f0b0ba0
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2018-11-06 12:53:45 -0300

    Agrego lectura de env var de version

--- conf/settings/base.py
+++ conf/settings/base.py
@@ -99,6 +99,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'conf.settings.local.export_vars',
             ],
         },
     },
