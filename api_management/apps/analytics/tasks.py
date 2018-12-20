d6dc6832fed69fe281c2a54767e672e282734a18
Alejandro Sobko <alejandro.sobko@gmail.com>
2018-12-20 14:07:19 -0300

    Aumento el timeout de generate_indicators_csv

--- api_management/apps/analytics/tasks.py
+++ api_management/apps/analytics/tasks.py
@@ -33,7 +33,7 @@ def generate_analytics_dump(analytics_date, task_logger=None):
         generate_csv(csv_generator, task_logger, api.name, analytics_date)
 
 
-@job('generate_indicators_csv', timeout=3600)
+@job('generate_indicators_csv', timeout=7200)
 def generate_indicators_csv(task_logger=None):
     task_logger = task_logger or IndicatorCsvGeneratorTask(created_at=timezone.now())
 
