861a45d1b7469cd3ce8ae84c4fc35c79e87a8ef8
Alejandro Sobko <alejandro.sobko@gmail.com>
2019-01-17 12:34:08 -0300

    Fix fecha al generar analytics
        refs:#175
    
        refs:#175

--- api_management/apps/common/utils.py
+++ api_management/apps/common/utils.py
@@ -1,2 +1,15 @@
+import datetime
+from django.utils import timezone
+
+
 def same(number):
     return number
+
+
+def as_locale_datetime(a_date):
+    return a_date.astimezone(timezone.get_current_timezone())
+
+
+def date_at_midnight(a_date):
+    locale_date = as_locale_datetime(a_date)
+    return datetime.datetime(locale_date.year, locale_date.month, locale_date.day)
