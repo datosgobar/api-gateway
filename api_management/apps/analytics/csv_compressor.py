b2fa1aae9f6622845670deb9ca1373715d57ffcf
Alejandro Sobko <alejandro.sobko@gmail.com>
2019-01-22 16:54:34 -0300

    Zippeamos por año
        refs:#174
    
        refs:#174

--- api_management/apps/analytics/csv_compressor.py
+++ api_management/apps/analytics/csv_compressor.py
@@ -1,6 +1,5 @@
 import zipfile
 
-from dateutil import relativedelta
 from django.conf import settings
 from django.core.files import File
 from django.utils import timezone
@@ -13,30 +12,32 @@ class CsvCompressor:
     def __init__(self, api_name):
         self.api_name = api_name
 
-    def zip_name(self):
-        return (timezone.now() - relativedelta.relativedelta(years=1)).strftime('analytics_%Y')
+    def zip_name(self, year):
+        return "analytics_{year}".format(year=year)
 
-    def last_year_file_names(self):
-        date_list = [timezone.now() - relativedelta.relativedelta(days=x) for x in range(0, 365)]
-        date_strings = map(lambda x: x.strftime('%Y-%m-%d'), date_list)
-        return list(map(lambda x: "analytics_{date}.csv".format(date=x), date_strings))
+    def full_zip_name(self, file_name):
+        return "{path}/{name}.zip".format(path=settings.MEDIA_ROOT, name=file_name)
 
-    def older_than_last_year(self):
-        file_names = self.last_year_file_names()
-        return CsvFile.objects.filter(type='analytics',
-                                      api_name=self.api_name).exclude(file_name__in=file_names)
+    def first_csv_year(self):
+        return int(CsvFile.objects.first().file_name[10:-10])
+
+    def csv_range_years(self):
+        return range(self.first_csv_year(), timezone.now().year + 1)
 
     def compress(self):
-        files = self.older_than_last_year()
-        self.perform_compression(files, self.zip_name())
-        # files.delete()
+        for year in self.csv_range_years():
+            files = CsvFile.objects.filter(type='analytics',
+                                           api_name=self.api_name,
+                                           file_name__icontains=year)
+            self.perform_compression(files, self.zip_name(year))
+            # self.delete_zipped_files(files)
 
     def perform_compression(self, csv_files, zip_name):
-        zip_file_name = "{path}/{name}.zip".format(path=settings.MEDIA_ROOT, name=zip_name)
+        zip_file_name = self.full_zip_name(zip_name)
         zip_file = zipfile.ZipFile(zip_file_name, 'w', zipfile.ZIP_DEFLATED)
 
         for csv_file in csv_files:
-            zip_file.write(csv_file.file.path)
+            zip_file.write(csv_file.file.path, arcname=csv_file.file_name)
 
         zip_file.close()
         ZipFile(file_name=zip_file_name, file=File(zip_file)).save()
