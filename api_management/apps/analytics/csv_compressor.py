06b0c0bcb6be2ca0a6047e6b9e90f9e6be1a4712
Alejandro Sobko <alejandro.sobko@gmail.com>
2019-01-22 15:29:26 -0300

    Generamos un zip con todos los csv anteriores a un año
        refs:#174
    
        refs:#174

--- api_management/apps/analytics/csv_compressor.py
+++ api_management/apps/analytics/csv_compressor.py
@@ -1,6 +1,8 @@
 import zipfile
 
 from dateutil import relativedelta
+from django.conf import settings
+from django.core.files import File
 from django.utils import timezone
 
 from api_management.apps.analytics.models import CsvFile, ZipFile
@@ -10,6 +12,9 @@ class CsvCompressor:
 
     def __init__(self, api_name):
         self.api_name = api_name
+
+    def zip_name(self):
+        return (timezone.now() - relativedelta.relativedelta(years=1)).strftime('analytics_%Y')
 
     def last_year_file_names(self):
         date_list = [timezone.now() - relativedelta.relativedelta(days=x) for x in range(0, 365)]
@@ -23,13 +28,15 @@ class CsvCompressor:
 
     def compress(self):
         files = self.older_than_last_year()
-        self.perform_compression(files)
-        files.delete()
+        self.perform_compression(files, self.zip_name())
+        # files.delete()
 
-    def perform_compression(self, csv_files):
+    def perform_compression(self, csv_files, zip_name):
+        zip_file_name = "{path}/{name}.zip".format(path=settings.MEDIA_ROOT, name=zip_name)
+        zip_file = zipfile.ZipFile(zip_file_name, 'w', zipfile.ZIP_DEFLATED)
+
         for csv_file in csv_files:
-            zip_file_name = "{name}.zip".format(name=csv_file.file_name[:-3])
-            zip_file = zipfile.ZipFile(zip_file_name, 'w', zipfile.ZIP_DEFLATED)
-            zip_file.write(csv_file.file.path, arcname=zip_file_name)
-            zip_file.close()
-            ZipFile(name=zip_file_name, file=zip_file).save()
+            zip_file.write(csv_file.file.path)
+
+        zip_file.close()
+        ZipFile(file_name=zip_file_name, file=File(zip_file)).save()
