779cd291a4702f55d6c129f2e800bcc5291e41e7
Alejandro Sobko <alejandro.sobko@gmail.com>
2019-01-23 18:00:24 -0300

    Refactor en cómo se crean y se borran archivos de tests
        refs:#174
    
        refs:#174

--- api_management/apps/analytics/test/csv_compressor_tests.py
+++ api_management/apps/analytics/test/csv_compressor_tests.py
@@ -21,26 +21,39 @@ def get_file_mock(file_name):
 
 
 def create_csv_file(name):
-    file_mock = get_file_mock(name)
-    CsvFile(type='analytics', api_name='series', file_name=name, file=file_mock).save()
-    return file_mock
+    with open(str(settings.MEDIA_ROOT + '/' + name), 'r+') as file:
+        CsvFile(type='analytics', api_name='series', file_name=name, file=File(file)).save()
+        return file
 
 
-def delete_files(file_names):
-    for name in file_names:
-        os.remove(name)
+@pytest.mark.django_db
+def delete_files():
+    for csv_file in CsvFile.objects.all():
+        os.remove(csv_file.file.path)
+
+    for zip_file in ZipFile.objects.all():
+        os.remove(zip_file.file_name)
 
 
+@pytest.fixture(autouse=True)
+def fixture_func():
+    yield
+    delete_files()
+
+
+@pytest.mark.django_db
 def test_zip_name():
     assert csv_compressor().zip_name(2018) == 'analytics_2018'
     assert csv_compressor().zip_name('2018') == 'analytics_2018'
 
 
+@pytest.mark.django_db
 def test_full_zip_name():
     full_name = "{path}/analytics_2018.zip".format(path=settings.MEDIA_ROOT)
     assert csv_compressor().full_zip_name('analytics_2018') == full_name
 
 
+@pytest.mark.django_db
 def test_years_from_csv_file_name():
     csv_file = CsvFile(api_name='series', file_name='analytics_2017-10-15.csv')
     assert csv_compressor().years_from_csv_file_name(csv_file) == '2017'
@@ -48,43 +61,40 @@ def test_years_from_csv_file_name():
 
 @pytest.mark.django_db
 def test_year_of_first_csv_file():
-    file_mock1 = create_csv_file('analytics_2017-10-15.csv')
-    file_mock2 = create_csv_file('analytics_2017-11-25.csv')
-    file_mock3 = create_csv_file('analytics_2018-10-15.csv')
+    create_csv_file('analytics_2017-10-15.csv')
+    create_csv_file('analytics_2017-11-25.csv')
+    create_csv_file('analytics_2018-10-15.csv')
 
     assert csv_compressor().year_of_first_csv_file() == 2017
-    delete_files([file_mock1.name, file_mock2.name, file_mock3.name])
 
 
 @pytest.mark.django_db
 def test_csv_range_years():
     current_year = timezone.now().year
-    file_mock1 = create_csv_file('analytics_2017-10-15.csv')
-    file_mock2 = create_csv_file('analytics_2017-11-25.csv')
-    file_mock3 = create_csv_file('analytics_2018-10-15.csv')
+    create_csv_file('analytics_2017-10-15.csv')
+    create_csv_file('analytics_2017-11-25.csv')
+    create_csv_file('analytics_2018-10-15.csv')
 
     assert csv_compressor().csv_range_years() == range(2017, current_year + 1)
-    delete_files([file_mock1.name, file_mock2.name, file_mock3.name])
 
 
 @pytest.mark.django_db
 def test_compress_single_file():
-    file_name = 'analytics_2018-08-15.csv'
+    file_name = 'analytics_2018-10-15.csv'
     csv_file = CsvFile(api_name='series', file_name=file_name, file=get_file_mock(file_name))
+    create_csv_file('analytics_2018-10-15.csv')
 
     csv_compressor().compress_single_file(csv_file)
 
     assert ZipFile.objects.count() == 1
     assert 'analytics_2018.zip' in ZipFile.objects.first().file_name
 
-    os.remove(ZipFile.objects.first().file_name)
-
 
 @pytest.mark.django_db
 def test_compress_all():
-    file_mock1 = create_csv_file('analytics_2017-10-15.csv')
-    file_mock2 = create_csv_file('analytics_2017-11-25.csv')
-    file_mock3 = create_csv_file('analytics_2018-10-15.csv')
+    create_csv_file('analytics_2017-10-15.csv')
+    create_csv_file('analytics_2017-11-25.csv')
+    create_csv_file('analytics_2018-10-15.csv')
 
     csv_compressor().compress_all()
 
@@ -92,8 +102,3 @@ def test_compress_all():
     assert 'analytics_2017.zip' in ZipFile.objects.all()[0].file_name
     assert 'analytics_2018.zip' in ZipFile.objects.all()[1].file_name
     assert 'analytics_2019.zip' in ZipFile.objects.all()[2].file_name
-
-    delete_files([file_mock1.name, file_mock2.name, file_mock3.name,
-                  ZipFile.objects.all()[0].file_name,
-                  ZipFile.objects.all()[1].file_name,
-                  ZipFile.objects.all()[2].file_name])
