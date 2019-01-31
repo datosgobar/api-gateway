73bf28a82a000a405d0b5445ed3a637475dca21f
Alejandro Sobko <alejandro.sobko@gmail.com>
2019-01-31 14:05:23 -0300

    Borro tests porque modifica el índice real
        refs:#166
    
        refs:#166

--- api_management/apps/analytics/test/elastic_search/index_query_tests.py
+++
@@ -1,9 +0,0 @@
-from unittest.mock import patch
-
-
-def test_index_query_after_save(query, httplogdata):
-    with patch('api_management.apps.analytics.elastic_search.query_index.index_query')\
-            as index_query_call:
-        query.api_data.httplog = httplogdata
-        query.save()
-        assert index_query_call.assert_called_once
