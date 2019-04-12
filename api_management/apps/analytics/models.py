4005d5e54979ea16dcdfe3302b31dddec43f60d0
Alejandro Sobko <alejandro.sobko@gmail.com>
2019-04-12 10:53:56 -0300

    Borro imports sin usar

--- api_management/apps/analytics/models.py
+++ api_management/apps/analytics/models.py
@@ -9,8 +9,6 @@ import requests
 from dateutil import relativedelta
 from django.conf import settings
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 from django.utils.translation import gettext_lazy as _
 from redis import Redis
 from solo.models import SingletonModel
