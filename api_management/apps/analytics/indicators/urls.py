66a57a7142f85de1278f15b55470eb9c53e62efd
Franco Scaccheri Cassanello <fcassanello@devartis.com>
2019-09-10 13:06:30 -0300

    linter fix

--- api_management/apps/analytics/indicators/urls.py
+++ api_management/apps/analytics/indicators/urls.py
@@ -1,9 +1,9 @@
-from api_management.apps.analytics.views import download_indicators_csv_view
 from django.conf.urls import url
 
+from api_management.apps.analytics.views import download_indicators_csv_view
 
 urlpatterns = [
     url(r'^indicadores-(?P<api_endpoint>[a-z]{1,30}.+).csv$',
-            download_indicators_csv_view,
-            name='download-indicators-csv')
+        download_indicators_csv_view,
+        name='download-indicators-csv')
 ]
